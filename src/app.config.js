ef2f089d06931bc92b6ecb73e4c86d12dcba8eac
HarshalR15 <115364412+HarshalR15@users.noreply.github.com>
2023-07-10 14:50:15 +0530

    Add files via upload

---
+++ src/app.config.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDdhCazZ3LpceuChe18hPG-eB4Wr76HkkM",
+    authDomain: "primadisscuss.firebaseapp.com",
+    databaseURL: "https://primadisscuss-default-rtdb.firebaseio.com",
+    projectId: "primadisscuss",
+    storageBucket: "primadisscuss.appspot.com",
+    messagingSenderId: "452987137622",
+    appId: "1:452987137622:web:4737f1c6f2757f117b55f7",
+    measurementId: "G-QE0EW6XRK2"
+  };
+  
+  
+  const cometChat ={
+      APP_ID: '240648cc862adda0',
+    AUTH_KEY: '83b28312a1c028a0a9f422201378a126f6e6cfe6',
+    REST_KEY: '57c8a999aec72891be11c38b16ea3a28b80c2c24',
+    APP_REGION: 'eu',
+  
+  }
+  
+  export { firebaseConfig, cometChat }
